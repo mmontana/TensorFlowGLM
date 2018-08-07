2d7c3ee9875b3b60f443962d6b4b8b865b3ace10
mattes <mattesm@zedat.fu-berlin.de>
2018-08-07 14:43:06 +0200

    fixed dimensionality issues

--- GLM/model.py
+++ GLM/model.py
@@ -1,42 +1,64 @@
 import tensorflow as tf
+import numpy as np
 
 class GLMBase(object):
     
-    def __init__(self,x_train,y_train,activation=None,loss=tf.losses.mean_squared_error,
-                 optim=tf.train.AdamOptimizer(.1), dropout= None):
+    def __init__(self, x_train, y_train,
+                 activation=None,
+                 loss=tf.losses.mean_squared_error,
+                 optim=tf.train.AdamOptimizer(.1), 
+                 dropout= None,
+                 weight_regularization = None):
             
+        if x_train.shape[0] != y_train.shape[0]:
+            raise ValueError("training and target data sample numbers must match")
+            
+        self._n_samples = x_train.shape[0]
         self._fit = False
         self._session = None
         self._x_train = x_train
         self._y_train = y_train
-        #get all class variables
-        
+        self.dropout = dropout     
         self._activation = activation
         
         self._loss = loss
         self._optim = optim
         
+        
+        
+        if self._x_train.ndim == 1:
+            x_dim = 1
+        else:
+            x_dim = self._x_train.shape[1]
+        if self._y_train.ndim == 1:
+            y_dim = 1
+        else:
+            y_dim = self._y_train.shape[1]
+            
         #TODO generalize dimensionality
-        self._W_init = tf.truncated_normal(shape=[1])
-        self._b_init = tf.truncated_normal(shape=[1])
+        self._W_init = tf.truncated_normal(shape=(x_dim,y_dim))
+        self._b_init = tf.truncated_normal(shape=(y_dim,))
         
         self._W = tf.Variable(self._W_init)
         self._b = tf.Variable(self._b_init)
         
-        self._x = tf.placeholder(dtype=tf.float32)
-        self._y = tf.placeholder(dtype=tf.float32)
-        self._y_lin = (self._W * self._x) + self._b
+        #self._x = tf.placeholder(tf.float32, (self._n_samples,x_dim))
+        #self._y = tf.placeholder(tf.float32, (self._n_samples,y_dim))
+        self._x = tf.placeholder(tf.float32)
+        self._y = tf.placeholder(tf.float32)
+
+        #self._y_lin = (self._x * self._W) + self._b
+        self._y_lin = (tf.matmul(self._x, self._W)) + self._b
         
         if self._activation is not None:
             self._y_pred = self._activation(self._y_lin) 
         else:
-            self._y_pred = self._y_lin_pred
+            self._y_pred = self._y_lin
             
         self._l = self._loss(self._y, self._y_pred)
-        
         self._train = self._optim.minimize(self._l)
     
-    def fit(self, n_steps):
+    def fit(self, n_steps, batch=False):
         #create session
         if not self._fit:
             self._session = tf.Session()
@@ -53,9 +75,20 @@ class GLMBase(object):
         #store weights?           
         pass
     
-    #TODO predict vs predict proba for logistic cases
-    def predict(self,x_test):
-        return self._session.run(self._y_pred,feed_dict={self._x:x_test})    
+    def predict(self, x_test, 
+                predict_classes=False,
+                batches=False,
+                shuffle_batches=True,
+                decimals=0):
+        
+        #dimension handling
+        if x_test.ndim == 1:
+            x_test = x_test.reshape(x_test.shape[0],1)
+        
+        y_out = self._session.run(self._y_pred,feed_dict={self._x:x_test})
+        if predict_classes:
+            y_out = np.round(y_out, decimals=decimals)
+        return y_out
     
     def fit_predict(self):
         pass
